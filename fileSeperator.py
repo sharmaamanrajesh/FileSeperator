4a80cc82eea917386fb5a003a3cd78b15a150832
ITS-AMANSHARMA <amansharma33@icloud.com>
2022-07-17 22:46:49 +0530

    .

---
+++ fileSeperator.py
@@ -0,0 +1,6 @@
+import os,shutil
+
+audioExtensions = ('.mp3','.m4a','.wav','.flac')
+imageExtensions = ('.jpeg','.jpg','.png','.svg','.gif')
+videoExtensions = ('.mp4','.mkv','.MKV','.flv','.mpeg')
+documentExtensions = ('.doc','.pages','.pdf','.txt')
