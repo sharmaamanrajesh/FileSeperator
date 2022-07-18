de9047eecfdaf60d518db9a2ef3bf34126e299ca
ITS-AMANSHARMA <amansharma33@icloud.com>
2022-07-18 21:59:24 +0530

    .

--- fileSeperator.py
+++ fileSeperator.py
@@ -1,11 +1,16 @@
 import os,shutil
 import types
 
-audioExtensions = ('.mp3','.m4a','.wav','.flac')
-imageExtensions = ('.jpeg','.jpg','.png','.svg','.gif')
-videoExtensions = ('.mp4','.mkv','.MKV','.flv','.mpeg')
-documentExtensions = ('.doc','.pages','.pdf','.txt')
-sourceCodeExtensions = ('.cpp','.c','.py','.java','.html','.css')
+DictExtensions = {
+    'audio_Extensions' : ('.mp3','.m4a','.wav','.flac'),
+    'image_Extensions' : ('.jpeg','.jpg','.png','.svg','.gif','.ai'),
+    'video_Extensions' : ('.mp4','.mkv','.MKV','.flv','.mpeg'),
+    'document_Extensions' : ('.doc','.pages','.pdf','.txt'),
+    'sourceCode_Extensions' : ('.cpp','.c','.py','.java','.html','.css'),
+    'dmg_Extensions' : ('.dmg'),
+    'other_Extensions' : ('.rar','.zip','.download','.'),
+}
+
 
 # Taking folderPath as an input from the user
 folderPath = input("Enter the path of the folder:\n")
@@ -19,4 +24,11 @@ def fileFinder(folder_path,file_extensions):
                 files.append(file)
     return files
 
-print(fileFinder(folderPath,imageExtensions))
+for extensiontypes, typeTuple in DictExtensions.items():
+    folder_name = 'My' + extensiontypes.split('_')[0] + 'Files'
+    folder_path = os.path.join(folderPath,folder_name)
+    os.mkdir(folder_path)
+    for item in (fileFinder(folderPath,typeTuple)):
+        item_path = os.path.join(folder_path,item)
+        new_item_path = os.path.join(folder_path,item)
+        shutil.move(item_path,new_item_path)
